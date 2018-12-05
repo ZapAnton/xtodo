cbfc1aec7756ebc1061795f8fd1865cacf9a09f2
ZapAnton <wc3ft2@gmail.com>
2018-12-05 13:55:01 +0300

    Added the track_dir argument

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,20 @@ extern crate toml;
 
 mod cmd;
 
-use clap::{App, ArgMatches, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
+use std::path::Path;
 
 fn init_app<'a>() -> ArgMatches<'a> {
     App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .author(env!("CARGO_PKG_AUTHORS"))
+        .arg(Arg::with_name("track_dir")
+             .short("d")
+             .long("track-dir")
+             .help("A path to the Exercism track repo. If not set, defaults to the current directory.")
+             .takes_value(true)
+             .default_value("."))
         .subcommand(
             SubCommand::with_name("outdated")
                 .about("List all outdated exercises on the current track"),
@@ -23,6 +30,8 @@ fn init_app<'a>() -> ArgMatches<'a> {
 }
 
 fn process_matches(matches: &ArgMatches) -> xtodo::Result<()> {
+    let track_dir = Path::new(matches.value_of("track_dir").unwrap());
+
     match matches.subcommand() {
         ("missing", _) => cmd::list_missing_exercises(),
 
